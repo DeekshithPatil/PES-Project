b0dd0f7961452dc477829a4561133c7c6be7549e
DeekshithPatil <deekurdy@gmail.com>
2021-12-06 22:14:33 -0700

    Added huffman python program to PC side

---
+++ PC_Side_Code/huffman.py
@@ -0,0 +1,120 @@
+# Huffman Coding in python
+
+string = ""
+f = open("logFiles.txt", "r")
+
+data = f.read()
+
+string = str(data)
+
+# Creating tree nodes
+class NodeTree(object):
+
+    def __init__(self, left=None, right=None):
+        self.left = left
+        self.right = right
+
+    def children(self):
+        return (self.left, self.right)
+
+    def nodes(self):
+        return (self.left, self.right)
+
+    def __str__(self):
+        return '%s_%s' % (self.left, self.right)
+
+
+# Main function implementing huffman coding
+def huffman_code_tree(node, left=True, binString=''):
+    if type(node) is str:
+        return {node: binString}
+    (l, r) = node.children()
+    d = dict()
+    d.update(huffman_code_tree(l, True, binString + '0'))
+    d.update(huffman_code_tree(r, False, binString + '1'))
+    return d
+
+
+# Calculating frequency
+freq = {}
+for c in string:
+    if c in freq:
+        freq[c] += 1
+    else:
+        freq[c] = 1
+
+freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
+
+nodes = freq
+
+while len(nodes) > 1:
+    (key1, c1) = nodes[-1]
+    (key2, c2) = nodes[-2]
+    nodes = nodes[:-2]
+    node = NodeTree(key1, key2)
+    nodes.append((node, c1 + c2))
+
+    nodes = sorted(nodes, key=lambda x: x[1], reverse=True)
+
+huffmanCode = huffman_code_tree(nodes[0][0])
+c_hdr = """\
+/*
+* Huffman codes
+*
+* NOTE: THIS FILE IS AUTO GENERATED -- Do not edit
+*/
+#ifndef HUFFMAN_H_
+#define HUFFMAN_H_
+
+#include <stdint.h>
+
+typedef struct
+{
+    unsigned char symbol;      // Symbol being coded
+    uint32_t code;             // Binary code for symbol
+    int code_bits;             // Number of bits in code
+}huffman_code_t;
+
+#define HUFF_CODE_END_SYMBOL  '\\0'"""
+
+print(c_hdr)
+
+minimum = 65535
+maximum = 0
+
+for (char, frequency) in freq:
+    length = len(huffmanCode[char])
+    if(length > maximum):
+        maximum = length
+    elif(length < minimum):
+        minimum = length
+
+print(f'#define HUFF_CODE_MIN_LENGTH ({minimum})')
+print(f'#define HUFF_CODE_MAX_LENGTH ({maximum})')
+print("")
+print("huffman_code_t huffman_code[] = {")
+
+hexNumber = 0
+count = 0
+
+for (char, frequency) in freq:
+#     print(' %-4r |%s' % (char, huffmanCode[char]))
+
+
+    hexNumber = hex(int((huffmanCode[char]),2))
+
+    if(char == "'"):
+        print("{'\\'',%s,%d}," % (hexNumber,len(huffmanCode[char])))
+    else:
+
+        print('{%r,%s,%d},' % (char, hexNumber,len(huffmanCode[char])))
+
+    # print('{%r,%s,%d},' % (char, hexNumber,len(huffmanCode[char])))
+
+    count = count + 1
+
+print("{HUFF_CODE_END_SYMBOL,0,0}};")
+
+print("")
+
+print("#endif /*HUFFMAN_H_*/")
---
+++ PC_Side_Code/main.c
@@ -0,0 +1,9 @@
+#include "huffman.h"
+#include <stdio.h>
+
+void main()
+{
+  // char c = '\'';
+  printf("\n%c %u %d\n",huffman_code[0].symbol,huffman_code[0].code,huffman_code[0].code_bits);
+  // printf("%c\n", c);
+}
